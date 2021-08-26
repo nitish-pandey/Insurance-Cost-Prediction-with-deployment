97fddb7ebc0e5c81e35eca303ace9b8b0f099e2b
Nitish Pandey <85100877+nitish-pandey@users.noreply.github.com>
2021-08-26 17:46:37 +0545

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,26 @@ import pandas as pd
 import numpy as np
 import joblib
 from flask import Flask ,render_templates
+
+
+
+app = Flask(__name__)
+
+
+model = joblib.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+
+def hello():
+    return render_template('home.html')
+
+@app.route('/',methods=['GET','POST'])
+
+def predict():
+  input_values = [float(x) for x in request.form.values()]
+  inp_features = [input_values]
+  prediction = model.predict(inp_features)
+  return render_template('home.html',output=prediction)
+  
+if __name__=='__main__':
+  app.run(debug=True)
